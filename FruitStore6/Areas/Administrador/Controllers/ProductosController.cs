bf7db57bb2da536280c529b3881ec59f38b52553
operador <operador@CCA5-04>
2022-11-17 16:54:02 -0600

    cambios

--- FruitStore6/Areas/Administrador/Controllers/ProductosController.cs
+++ FruitStore6/Areas/Administrador/Controllers/ProductosController.cs
@@ -2,16 +2,39 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using FruitStore6.Areas.Administrador.Models;
+using FruitStore6.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace FruitStore6.Areas.Administrador.Controllers
 {
+    [Area("Administrador")]
     public class ProductosController : Controller
     {
-        [Area("Administrador")]
-        public IActionResult Index()
+        private readonly fruteriashopContext context;
+
+        public ProductosController(fruteriashopContext context)
         {
-            return View();
+            this.context = context;
+        }
+
+        public IActionResult Index(IndexProductosViewModel vm)
+        {
+
+
+            if (vm.IdCategoria == 0)//No eligieron categoria, selecciono todo
+            {
+                vm.Productos = context.Productos.Include(x=>x.IdCategoriaNavigation).OrderBy(x => x.Nombre);
+            }
+            else
+            {
+                vm.Productos = context.Productos.Where(x => x.IdCategoria == vm.IdCategoria).Include(x=>x.IdCategoriaNavigation).OrderBy(x=>x.Nombre);
+            }
+
+            vm.Categorias = context.Categorias.OrderBy(x => x.Nombre);
+
+            return View(vm);
         }
 
         public IActionResult Agregar()
